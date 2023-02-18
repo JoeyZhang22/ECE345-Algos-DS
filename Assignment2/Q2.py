315582a6e758b042c230a2318b9b4ceb7136dcb0
Christopher Tong <christong@Christophers-MacBook-Pro-2.local>
2023-02-18 15:26:24 -0500

    updated Q2 file

--- Assignment2/Q2.py
+++ Assignment2/Q2.py
@@ -8,10 +8,6 @@ with open('passwords.txt') as f:
 #print(len(lines))
 #n=len(lines) #number of keys from passwords.txt
 
-#chained hash table implementation python
-#n keys, but m slots
-# load factor = n/m = keys/slots
-
 def display_hash(hashTable):
       
     for i in range(len(hashTable)):
@@ -24,24 +20,13 @@ def display_hash(hashTable):
         print()
 
 
-
-#! is it okay to make the hash table the same size as the passwords file? I thought we were suppoesd to make smaller
-# for Q2 we need to vary size of table given an input of 1000 so it cnanot always be that way
-
-# Hashing Function to return 
-# key for every value.
-def Hashing(keyvalue):
-    return keyvalue % len(HashTable)
-#need specific hash function that is based on the string input so that if we have a given input it will give us the same 
-#key every time so we can easily look up the value instead of using a forloop
-
-
 #key every time so we can easily look up the value instead of using a forloop
 def djb2(stringinput,HashTable):
+    #return hash(stringinput) % len(HashTable)
     hash=5381
-    #for every letter in the string
+     #for every letter in the string
     for c in stringinput:
-        hash=(hash*33)+ord(c)
+         hash=(hash*33)+ord(c)
     return hash % len(HashTable)
 
 #create hashtable as a nested list
@@ -50,13 +35,11 @@ HashTable = [[] for _ in range(len(lines))]
 #even if table has m=n, there is chance of remaping
 
 
-
 # Insert Function to add
 # values to the hash table
 def insert(Hashtable, keyvalue, value):
     #!assuming the key value is just the index of the value in lines
-    hash_key = Hashing(keyvalue)
-    Hashtable[hash_key].append(value)
+    Hashtable[keyvalue].append(value)
   
 # MAIN FUNCTION
 def load_table(HashTable,lines):
@@ -65,7 +48,7 @@ def load_table(HashTable,lines):
             key=djb2(lines[i],HashTable)
             insert(HashTable,key,lines[i])
 
-    #display_hash(HashTable) 
+    display_hash(HashTable) 
 
 def check_exist(string, HashTable):
     temp_key=djb2(string,HashTable)  #creates key for string
@@ -82,7 +65,6 @@ def check_exist(string, HashTable):
                 return True #password exists therefore not valid
 
         return False #if you go through the linkedlist and nothing is matched then return True
-
 
 
 def check_pass(string, HashTable,lines):
@@ -103,10 +85,18 @@ def check_pass(string, HashTable,lines):
         print('INVALID') 
         
 
-#print(hash('3UpuhkPqTYyj'))
 
+#LOADS PASSWORD FILE
 load_table(HashTable,lines)
-check_pass('nF33xIW',HashTable,lines)
+collisioncounter=0
+
+for i in HashTable:
+    if len(i)>1:
+        collisioncounter+= len(i)-1
+
+print('number of collissions is',collisioncounter)
+
+#check_pass('123456',HashTable,lines)
 
 #WIx33Fn
 #nF33xIW
